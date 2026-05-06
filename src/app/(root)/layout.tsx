a52ee2b5c087d6fa464016890eeaffea35e0869c
rahat9016 <minhajurrohoman9016@gmail.com>
2026-05-06 17:45:03 +0600

    feat: wrap Header component in Suspense to handle asynchronous loading states

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -4,13 +4,16 @@ import { CartDrawerProvider } from "@/src/components/cart/CartProvider";
 import FloatingCartButton from "@/src/components/cart/FloatingCartButton";
 import Footer from "@/src/components/layout/rootLayout/Footer";
 import Header from "@/src/components/layout/rootLayout/Header/Header";
+import { Suspense } from "react";
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <CartDrawerProvider>
       <div>
         <InternetStatus />
-        <Header />
+        <Suspense fallback={<div className="h-20" />}>
+          <Header />
+        </Suspense>
         {children}
         <Footer />
         <FloatingCartButton />
