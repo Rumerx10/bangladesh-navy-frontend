d280f0d64564be02f2f78da11ab5a2f8ddaaa92f
rahat9016 <minhajurrohoman9016@gmail.com>
2026-07-29 14:58:21 +0600

    feat: add Dockerfile and environment configuration files for deployment

--- src/config/envConfig.ts
+++ src/config/envConfig.ts
@@ -1,6 +1,5 @@
-export const baseURL = "http://202.51.176.30:8003/api/v1";
-// export const baseURL =
-// process.env.NEXT_PUBLIC_API_BASE_URL || "http://192.168.100.244:8001/api/v1";
+export const baseURL =
+  process.env.NEXT_PUBLIC_API_BASE_URL || "http://202.51.176.30:8003/api/v1";
 
 export const getBaseUrl = (): string => {
   return baseURL;
